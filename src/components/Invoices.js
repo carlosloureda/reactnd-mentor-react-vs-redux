ffcab03db300a007f7ae07be5d632696ae72b375
Carlos Loureda <balanzeneto@gmail.com>
2019-05-09 06:43:10 +0200

    added connect and provider from react-redux

--- src/components/Invoices.js
+++ src/components/Invoices.js
@@ -1,39 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
 import "./Invoices.css";
-import ReduxContext from "../context";
 
 const Invoices = props => {
+  const { userName } = props;
   return (
-    <ReduxContext.Consumer>
-      {store => {
-        const { userName } = store.getState();
-        return (
-          <div>
-            <h1>
-              Invoices page for <strong>{userName}</strong>
-            </h1>
-            <div>
-              <p>
-                On this page we would be showing user invoices, just an
-                scheleton
-              </p>
-              <div className="invoices">
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-                <img src="/img/invoices.png" alt="Sample invoice" />
-              </div>
-            </div>
-          </div>
-        );
-      }}
-    </ReduxContext.Consumer>
+    <div>
+      <h1>
+        Invoices page for <strong>{userName}</strong>
+      </h1>
+      <div>
+        <p>On this page we would be showing user invoices, just an scheleton</p>
+        <div className="invoices">
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+          <img src="/img/invoices.png" alt="Sample invoice" />
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default Invoices;
+Invoices.propTypes = {
+  userName: PropTypes.string.isRequired
+};
+
+export default connect(state => ({ userName: state.userName }))(Invoices);
--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,29 +1,23 @@
 import React, { Component } from "react";
 import Header from "./Header";
 // import Footer from "./Footer";
-import ReduxContext from "../../context";
 
 export default class MainLayout extends Component {
   render() {
+    const { userName, avatar } = this.props;
+
     return (
-      <ReduxContext.Consumer>
-        {store => {
-          const { userName, avatar } = store.getState();
-          return (
-            <div>
-              <Header userName={userName} avatar={avatar} />
-              <main>{this.props.children}</main>
-              {/* <Footer /> */}
-              <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                crossOrigin="anonymous"
-              />
-            </div>
-          );
-        }}
-      </ReduxContext.Consumer>
+      <div>
+        <Header userName={userName} avatar={avatar} />
+        <main>{this.props.children}</main>
+        {/* <Footer /> */}
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+      </div>
     );
   }
 }
